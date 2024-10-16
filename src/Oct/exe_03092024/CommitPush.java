a63c3ec627c4ca024ccf0cdbc73be102b46df960
This Pc <nitishkapil16@gmail.com>
2024-10-16 16:30:04 +0530

    My files

--- src/Oct/exe_03092024/CommitPush.java
+++ src/Oct/exe_03092024/CommitPush.java
@@ -5,8 +5,12 @@ public class CommitPush {
     public static void main(String[] args) {
       int a = 0X12a;
       String name="123";
+      int age= Integer.parseInt(args[2]);
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
         System.out.println(8%5);
-        System.out.println(a);
+        System.out.println(age);
         System.out.println(21> 22);
     }
 
---
+++ src/Task/InputUser.java
@@ -0,0 +1,12 @@
+package Task;
+
+public class InputUser {
+    public static void main(String[] args) {
+        int age= Integer.parseInt(args[0]);
+
+        System.out.println(args[2]);
+        System.out.println(args[1]);
+        System.out.println(age);
+
+    }
+}
