b75c7e85cc583d8ed69dcb60bc6e5c547301bade
This Pc <nitishkapil16@gmail.com>
2024-10-16 16:52:31 +0530

    My files

--- src/Task/Argument.java
+++ src/Task/Argument.java
@@ -1,12 +1,12 @@
 package Task;
 
-public class InputUser {
+public class Argument {
     public static void main(String[] args) {
         int age= Integer.parseInt(args[0]);
-
+        System.out.println(age>12?"allowed to goa" : "Not allowed");
         System.out.println(args[2]);
         System.out.println(args[1]);
         System.out.println(age);
-
+        System.out.printf("%s %s", args[0], args[1]);
     }
 }
---
+++ src/Task/LabOf_Scanner.java
@@ -0,0 +1,11 @@
+package Task;
+
+import java.util.Scanner;
+
+public class LabOf_Scanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+         int age = scanner.nextInt();
+        System.out.println(age>34?"allowed to go goa" : "allowed not to goa");
+    }
+}
